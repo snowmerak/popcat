6ed8132d4dd555f09c39fe0c98a704555c1e1dc7
snowmerak <go_dev@kakao.com>
2021-03-13 20:40:01 +0900

    make popcat

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	const open = 'images/open.png'
+	const close = 'images/close.png'
+	const sound = 'sounds/pop.mp3'
+
+	let imgSrc = close
+	let isPopping = false
+	let pop = new Audio(sound)
+	pop.volume = 1
+
+	function click() {
+		if (isPopping == true) {
+			return;
+		}
+		pop.play()
+		imgSrc = open
+		isPopping = true
+		window.setTimeout(() => {
+			imgSrc = close
+			isPopping = false
+		}, 100)
+	}
+</script>
+
+<main>
+	<img src={imgSrc} alt="popcat" on:click={click} />
+</main>
+
+<style>
+	img {
+		background-color: black;
+		width: 100%;
+	}
+</style>
